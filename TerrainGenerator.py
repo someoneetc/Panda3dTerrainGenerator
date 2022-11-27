74cdf2c1480f7b41e2ef309c52742daada098bd0
michele <gmvalfre@yahoo.it>
2022-11-27 19:05:53 +0100

    first working version

--- TerrainGenerator.py
+++ TerrainGenerator.py
@@ -9,16 +9,54 @@ import time
 import yaml
 import os 
 
+from panda3d.core import GeoMipTerrain, TextureStage, TexGenAttrib, Shader
+
+def loadTerrain(path):
+
+    #load terrain data
+    terrain_file = open(os.path.join(path,'map.json'))
+    terrain_data = json.loads(terrain_file.read())
+    terrain_file.close()
+
+    #Generate the terrain using GeoMipTerrain
+    terrain = GeoMipTerrain('terrain')
+    terrain.setHeightfield(terrain_data['heightmap'])
+    terrain.setBruteforce(True)
+    tRoot = terrain.getRoot()
+    tRoot.setTexGen(TextureStage.getDefault(),TexGenAttrib.MWorldPosition)
+    tRoot.setTexScale(TextureStage.getDefault(),1.0/terrain_data['shape'][0],1.0/terrain_data['shape'][1])
+    tRoot.setScale(1,1,terrain_data["z_scale"])
+    terrain.generate()
+
+    ts_idx = 0
+    for texture in terrain_data['textures']:
+        ts = TextureStage('gnd' + str(ts_idx))
+        ts.setSort(ts_idx)
+        tRoot.setTexture(ts,loader.loadTexture(terrain_data['textures'][ts_idx]))
+        ts_idx+=1
+
+
+    tRoot.setShaderInput("Heightmap",loader.loadTexture(terrain_data['heightmap']))
+    shader = Shader.load(vertex=terrain_data['vertex_shader'],
+                         fragment=terrain_data['fragment_shader'],
+                         lang=Shader.SL_GLSL)
+
+    tRoot.setShader(shader)
+    terrain.update()
+
+    return terrain, tRoot
+
 
 MAPMAKER_PATH = 'mapmaker/build/bin/mapmaker/mapmaker'
+SHADERS = 'shaders'
 
-def generateTerrain(shape,z_scale,texture_paths,name):
-    heightmap_path = os.path.join(name,"heightmap.pnm")
-    json_path = os.path.join(name,"map.json")
-    if os.path.exists(name):
-        print(repr(name), " directory already exists!")
+def generateTerrain(shape,z_scale,texture_paths,path):
+    heightmap_path = os.path.join(path,"heightmap.pnm")
+    json_path = os.path.join(path,"map.json")
+    if os.path.exists(path):
+        print(repr(path), " directory already exists!")
         return
-    os.mkdir(name)
+    os.mkdir(path)
     with open('tmp.yml','w+') as inputFile:
         """
         cfg = {
@@ -87,10 +125,13 @@ def generateTerrain(shape,z_scale,texture_paths,name):
     #generate json
     json_data = {
                 'heightmap': heightmap_path,
+                'shape': shape,
                 'z_scale': z_scale,
                 'textures': texture_paths,
+                'vertex_shader': os.path.join(SHADERS,'terrain.vert'),
+                'fragment_shader': os.path.join(SHADERS,'terrain.frag'),
             }
 
     json_file = open(json_path,"w+")
-    json_file.write(json.dumps(json_data))
+    json_file.write(json.dumps(json_data,indent=4))
     json_file.close()
--- shaders/terrain.vert
+++ shaders/terrain.vert
@@ -11,14 +11,10 @@ in vec4 p3d_Vertex;
 in vec4 p3d_Normal;
 in vec2 p3d_MultiTexCoord0;
 in vec2 p3d_MultiTexCoord1;
-in vec2 p3d_MultiTexCoord2;
-in vec2 p3d_MultiTexCoord3;
 
 // Output to fragment shader
 out vec2 texcoord0;
 out vec2 texcoord1;
-out vec2 texcoord2;
-out vec2 texcoord3;
 
 varying vec4 vertex;
 
@@ -26,7 +22,5 @@ void main() {
   gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
   texcoord0 = p3d_MultiTexCoord0;
   texcoord1 = p3d_MultiTexCoord1;
-  texcoord2 = p3d_MultiTexCoord2;
-  texcoord3 = p3d_MultiTexCoord3;
   vertex = p3d_Vertex;
 }
